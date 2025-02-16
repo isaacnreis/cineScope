c07d649ec54ee303eeb3c13780d06a7f143d29f9
Isaac N Reis <isaac.reis@teknisa.com>
2025-02-16 18:48:54 -0300

    Corrigindo import desnecessário do React

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/index.tsx";
 import Favorites from "./pages/Favorites/index.tsx";
